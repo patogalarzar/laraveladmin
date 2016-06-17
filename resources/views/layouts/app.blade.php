e2e97e55bebb19313847ea7e9c357f699d9de102
Patricio <patricio.galarza.r@gmail.com>
2016-06-17 17:31:45 -0500

    progres bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,11 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
         <![endif]-->
+
+        <!-- pace - page load progress bar -->
+        <script src="{{ asset('/pace/pace.min.js') }}"></script>
+        <link href="{{ asset('/pace/theme/pace-nimimal-theme.css') }}" rel="stylesheet" />
+
     </head>
     <body class="skin-blue">
         <div class="wrapper">
